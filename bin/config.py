1808f036bd50fec065d4503aab807e813e9dd6ee
Josh Ausborne <joshausborne@gmail.com>
2024-05-09 23:55:22 -0400

    set basic config files

---
+++ bin/config.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import json
+import psycopg2
+import os
+import sys
+
+base_dir = 'project_path/repo_directory/' # NEEDS UPDATING TO REFLECT YOUR ENVIRONMENT
+
+conf_dir = os.path.join(base_dir,'conf/')
+data = os.path.join(base_dir,'data/')
+kml_dir = os.path.join(base_dir,'kml/')
+boundaries = os.path.join(kml_dir,'boundaries/')
+flytos = os.path.join(kml_dir,'flytos/')
+placemarks = os.path.join(kml_dir,'placemarks/')
+
+
+# Import DB connection credentials from nps_db_connect.json
+
+with open(conf_dir + 'nps_db_connect.json','r') as f:
+    creds = json.load(f)
+
+host = creds['host']
+dbname = creds['dbname']
+user = creds['user']
+passwd = creds['passwd']
+
+conn_string = f"host='{host}' dbname='{dbname}' user='{user}' password='{passwd}'"
+conn = psycopg2.connect(conn_string)
+cursor = conn.cursor()
